bf0d7dbbcedbcd35b8e800516a837be7b87436f7
Fateh-Dev <58635504+Fateh-Dev@users.noreply.github.com>
2021-02-13 22:56:26 +0100

    Initial commit

---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss'],
+})
+export class HomePageComponent implements OnInit {
+  // displayedCol: string[] = [];
+  // data: MatTableDataSource<any> = new MatTableDataSource();
+
+  //TODO Profile Interface
+  constructor() {}
+  ngOnInit(): void {}
+}
